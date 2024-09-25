9af96ce0e9c4fd4024c389d33f9cbff500b5dffd
CodeMaker46 <shikshak013@gmail.com>
2024-09-25 18:22:25 +0530

    to resolve connection of convex and clerk

--- convex/http.ts
+++ convex/http.ts
@@ -1,26 +1,14 @@
-// create a clerk user -> create a podcast -> get all podcasts
+import type { WebhookEvent } from "@clerk/nextjs/server";
 import { httpRouter } from "convex/server";
-import { httpAction } from "./_generated/server";
-import { internal } from "./_generated/api";
-import type { WebhookEvent } from "@clerk/backend";
 import { Webhook } from "svix";
 
-function ensureEnvironmentVariable(name: string): string {
-  const value = process.env[name];
-  if (value === undefined) {
-    throw new Error(`missing environment variable ${name}`);
-  }
-  return value;
-}
-
-const webhookSecret = ensureEnvironmentVariable("CLERK_WEBHOOK_SECRET");
+import { internal } from "./_generated/api";
+import { httpAction } from "./_generated/server";
 
 const handleClerkWebhook = httpAction(async (ctx, request) => {
   const event = await validateRequest(request);
   if (!event) {
-    return new Response("Error occured", {
-      status: 400,
-    });
+    return new Response("Invalid request", { status: 400 });
   }
   switch (event.type) {
     case "user.created":
@@ -28,7 +16,7 @@ const handleClerkWebhook = httpAction(async (ctx, request) => {
         clerkId: event.data.id,
         email: event.data.email_addresses[0].email_address,
         imageUrl: event.data.image_url,
-        name: event.data.first_name!,
+        name: event.data.first_name as string,
       });
       break;
     case "user.updated":
@@ -50,32 +38,31 @@ const handleClerkWebhook = httpAction(async (ctx, request) => {
 });
 
 const http = httpRouter();
+
 http.route({
-  path: "/clerk-users-webhook",
+  path: "/clerk",
   method: "POST",
   handler: handleClerkWebhook,
 });
 
-async function validateRequest(
+const validateRequest = async (
   req: Request
-): Promise<WebhookEvent | undefined> {
+): Promise<WebhookEvent | undefined> => {
+  // key note : add the webhook secret variable to the environment variables field in convex dashboard setting
+  const webhookSecret = process.env.CLERK_WEBHOOK_SECRET!;
+  if (!webhookSecret) {
+    throw new Error("CLERK_WEBHOOK_SECRET is not defined");
+  }
   const payloadString = await req.text();
-
+  const headerPayload = req.headers;
   const svixHeaders = {
-    "svix-id": req.headers.get("svix-id")!,
-    "svix-timestamp": req.headers.get("svix-timestamp")!,
-    "svix-signature": req.headers.get("svix-signature")!,
+    "svix-id": headerPayload.get("svix-id")!,
+    "svix-timestamp": headerPayload.get("svix-timestamp")!,
+    "svix-signature": headerPayload.get("svix-signature")!,
   };
   const wh = new Webhook(webhookSecret);
-  let evt: Event | null = null;
-  try {
-    evt = wh.verify(payloadString, svixHeaders) as Event;
-  } catch (_) {
-    console.log("error verifying");
-    return;
-  }
-
-  return evt as unknown as WebhookEvent;
-}
+  const event = wh.verify(payloadString, svixHeaders);
+  return event as unknown as WebhookEvent;
+};
 
 export default http;
